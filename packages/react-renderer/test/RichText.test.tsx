59494874b8857564bf06f350ed8f6013f2eb046b
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2022-01-11 15:17:20 -0300

    test(react): adjust spacing and entities

--- packages/react-renderer/test/RichText.test.tsx
+++ packages/react-renderer/test/RichText.test.tsx
@@ -66,7 +66,7 @@ describe('@graphcms/rich-text-react-renderer', () => {
     expect(container).toMatchInlineSnapshot(`
       <div>
         <h2>
-
+          
           <a
             href="https://graphcms.com"
           >
@@ -74,7 +74,7 @@ describe('@graphcms/rich-text-react-renderer', () => {
           </a>
         </h2>
         <h2>
-
+          
           <a
             href="https://graphcms.com"
           >
@@ -417,17 +417,7 @@ describe('@graphcms/rich-text-react-renderer', () => {
 
     const { container } = render(<RichText content={content} />);
 
-    expect(container).toMatchInlineSnapshot(`
-      <div>
-        <p>
-          Hello,
-          <br />
-          My name is joão pedro,
-          <br />
-          I'm testing a bug
-        </p>
-      </div>
-    `);
+    expect(container).toMatchSnapshot();
   });
 });
 
@@ -870,7 +860,7 @@ describe('custom embeds and assets', () => {
           >
             <p>
               Your browser doesn't support HTML5 video. Here is a
-
+               
               <a
                 href="https://media.graphcms.com/7M0lXLdCQfeIDXnT2SVS"
               >
