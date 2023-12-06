232e4da8439de1aa1e8770b541825e8f3d288a44
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2023-12-06 11:18:18 -0300

    feat(html-to-slate-ast): add htmlToSlateASTSync function (#115)

---
+++ packages/html-to-slate-ast/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig(options => ({
+  entry: ['src/index.ts'],
+  tsconfig: 'tsconfig.build.json',
+  minify: !options.watch,
+  splitting: true,
+  sourcemap: true,
+  dts: true,
+  treeshake: true,
+  clean: true,
+  format: ['esm', 'cjs'],
+}));
